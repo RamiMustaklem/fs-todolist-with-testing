23e54ec6381c7f9b6a56cce002956000bf9b6f96
Rami Mustaklem (mr8118) <mr8118@intl.att.com>
2023-01-27 15:50:00 +0200

    Add flowbite to tailwind configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   darkMode: ['class', '[data-mode="dark"]'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+  ],
 }
